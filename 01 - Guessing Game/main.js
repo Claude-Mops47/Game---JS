4a6f1fd5ea83794419d160fb81c540314a88d300
Claude-Mops47 <mopenobiaclaude@gmail.com>
2022-11-15 14:21:32 +0100

    ok

--- 01 - Guessing Game/main.js
+++ 01 - Guessing Game/main.js
@@ -1,19 +1,19 @@
-// function guessNumber() {
-//   let playerGuess;
-//   const numberToGuess = Math.ceil(Math.random() * 10);
+function guessNumber() {
+  let playerGuess;
+  const numberToGuess = Math.ceil(Math.random() * 10);
 
-//   prompt("Devinez le nombre entre 1 et 10 inclus.");
+  prompt("Devinez le nombre entre 1 et 10 inclus.");
 
-//   while (playerGuess != numberToGuess) {
-//     playerGuess = prompt(
-//       "Echec! Essayez à nouveau de deviner le nombre (entre 1 et 10 inclus)."
-//     );
-//   }
-//   alert(`Felicitation! Le nombre était ${numberToGuess}!`);
-// }
-// guessNumber();
+  while (playerGuess != numberToGuess) {
+    playerGuess = prompt(
+      "Echec! Essayez à nouveau de deviner le nombre (entre 1 et 10 inclus)."
+    );
+  }
+  alert(`Felicitation! Le nombre était ${numberToGuess}!`);
+}
 
 const fs = require("fs");
+const { exit } = require("process");
 const rawdata = fs.readFileSync("animals.json");
 const animals = JSON.parse(rawdata);
 
