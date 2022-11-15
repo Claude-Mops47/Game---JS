16ccbd3bca6aab87de85d595f30bf1120c2f30ab
Claude-Mops47 <mopenobiaclaude@gmail.com>
2022-11-15 12:39:52 +0100

    correction

--- 01 - Guessing Game/main.js
+++ 01 - Guessing Game/main.js
@@ -13,43 +13,6 @@
 // }
 // guessNumber();
 
-// const scene = new THREE.Scene();
-// const camera = new THREE.PerspectiveCamera(
-//   75,
-//   innerWidth / innerHeight,
-//   0.1,
-//   1000
-// );
-// const renderer = new THREE.WebGLRenderer();
-// console.log(scene);
-// console.log(camera);
-// console.log(renderer);
-
-// renderer.setSize(innerWidth, innerHeight);
-// renderer.setPixelRadio(devicePixelRatio);
-// document.body.appendChild(renderer.domElement);
-
-// const boxGeometry = new THREE.boxGeometry(1, 1, 1);
-// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-// console.log(boxGeometry);
-// console.log(material);
-
-// const mesh = new THREE.Mesh(boxGeometry, material);
-// console.log(mesh);
-
-// scene.add(mesh);
-
-// camera.position.z = 5;
-
-// function animate() {
-//   requestAnimationFrame(animate);
-//   renderer.render(scene, camera);
-//   mesh.rotation.x += 0.01;
-//   mesh.rotation.y += 0.01;
-// }
-
-// animate();
-
 const fs = require("fs");
 const rawdata = fs.readFileSync("animals.json");
 const animals = JSON.parse(rawdata);
